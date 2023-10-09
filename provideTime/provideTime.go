a4bb58ac43ab0564c3a4f6087c8e94d06d2cc174
Andy Peralta <aperalta686@gmail.com>
2023-10-09 17:37:29 -0400

    Prompt user for time zone

--- provideTime/provideTime.go
+++ provideTime/provideTime.go
@@ -1,8 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func changeTimeZone(dst bool, zone string) *time.Location {
+	//time difference due to daylight savings
+	tDiff := 0
+	var loc *time.Location
+	if dst {
+		tDiff = 1
+	}
+
+	switch zone {
+	case "EST":
+		loc = time.FixedZone("EST", (-5+tDiff)*3600)
+	case "PST":
+		loc = time.FixedZone("PST", (-8+tDiff)*3600)
+	case "CST":
+		loc = time.FixedZone("CST", (-6+tDiff)*3600)
+	default:
+		loc = time.FixedZone("UTC", 0)
+	}
+	return loc
+}
 
 func main() {
-	//Boiler Plate
-	fmt.Println("Hello World")
+	//time format
+	var format string
+	fmt.Println("What format would you'd like the time")
+	fmt.Println("EST, PST, OR CST: ")
+	fmt.Scan(&format)
+
+	currTime := time.Now().UTC()
+	daylight := time.Now().IsDST()
+
+	//The Newly Formatted Time
+	newLoc := changeTimeZone(daylight, format)
+	newTime := currTime.In(newLoc).Format(time.RFC1123)
+	fmt.Println(newTime)
 }
