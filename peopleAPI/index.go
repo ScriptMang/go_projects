a424d26eb99c93db1dbfd79408b2d9cb8c36b4f5
Andy Peralta <aperalta686@gmail.com>
2023-11-04 16:02:48 -0400

    Query postgres via go

--- peopleAPI/index.go
+++ peopleAPI/index.go
@@ -1,9 +1,42 @@
 package main
 
 import (
+"context"	
  "fmt"
+ "os"
+
+ "github.com/jackc/pgx/v5"
 )
 
 func main() {
-	fmt.Println("HelloWorld")
+	
+	
+    uri := "postgres://user:secret@ipaddr:5432/databasename"
+	os.Setenv("DATABASE_URL", uri)
+    
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to a database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	
+	sqlCMD := "SELECT * FROM People"
+	
+	rows, _ := conn.Query(context.Background(), sqlCMD)
+	
+
+    var  fname, lname string
+	var id, age int64
+	_, err = pgx.ForEachRow(rows, []any{&id, &fname, &lname, &age},  func() error {
+		fmt.Printf("id: %v, fname: %v, lname: %v, age:%v\n", id,fname,lname,age)
+		return nil
+	})
+	
+	if err != nil {
+		fmt.Printf("ForEachRow error: %v\n", err)
+		os.Exit(1)
+	}
+
 }
