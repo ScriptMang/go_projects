0f48d8f047a3eec5444aab5c10c4a1308393fc04
Andy Peralta <aperalta686@gmail.com>
2023-11-06 16:42:24 -0500

    e

--- peopleAPI/index.go
+++ peopleAPI/index.go
@@ -9,76 +9,66 @@ import (
 )
 
  type Responder interface {
- 	Response() error
+ 	Response(conn *pgx.Conn) error
  }
 
-// sqlCMD is only option I provide
-// The rest I ask for the result back
- type Option struct {
- 	sqlCMD string
- 	id, age int
- 	fname, lname string
- 	optionId int // number of fields I expect back
- }
+type Fields struct {
+	fname, lname string
+	id, age int
+}
 
+type Option struct {
+ 	qry string
+ 	optionID int
+ 	Fields
+}
 
+func createOption (qry string, id int) Option {
+	return Option{qry, id, Fields{"","",0,0}}
+}
 
-func (opt Option) Response(conn *pgx.Conn) error {
-	qry := opt.sqlCMD
-	fmt.Printf("SQLCMD:\n%s\n", qry)
-	rows, _ := conn.Query(context.Background(), qry)
-	var err error 
-	
-	switch opt.optionId {
-	 case 1:
-	  fmt.Printf("OptionId 1 was given\n")	
-	  _, err = pgx.ForEachRow(rows, []any{&opt.fname, &opt.lname}, func() error {
-		  fmt.Printf("fname: %v, lname: %v\n", opt.fname,opt.lname)
-		  return nil
-	  })
-	  case 2:
-		  _, err = pgx.ForEachRow(rows,[]any{
-		  	&opt.fname, 
-		  	&opt.lname, 
-		  	&opt.age,}, func() error {
-			fmt.Printf("fname: %v, lname: %v, age:%v\n", opt.fname,opt.lname,opt.age)
-			return nil
-	  })
-	  case 3:
-	  _, err = pgx.ForEachRow(rows, []any{&opt.id, &opt.fname, &opt.lname, &opt.age}, func() error {
-		  fmt.Printf("id: %v, fname: %v, lname: %v, age:%v\n", opt.id,opt.fname,opt.lname,opt.age)
-		  return nil
-	  })
-	  case 4:
-	  _, err = pgx.ForEachRow(rows, []any{&opt.id, &opt.fname, &opt.lname, &opt.age}, func() error {
-		  fmt.Printf("id: %v, fname: %v, lname: %v, age:%v\n", opt.id,opt.fname,opt.lname,opt.age)
-		  return nil
-	  })
-	  case 5:
-	  _, err = pgx.ForEachRow(rows, []any{&opt.id, &opt.fname, &opt.lname, &opt.age}, func() error {
-		  fmt.Printf("id: %v, fname: %v, lname: %v, age:%v\n", opt.id,opt.fname,opt.lname,opt.age)
-		  return nil
-	  })					
-	}
-
-	if err != nil {
+func verifyForEachRowError(err error) error {
+     if err != nil {
 		fmt.Printf("ForEachRow error: %v\n", err)
 		os.Exit(1)
 	}
 	return nil
 }
 
-func createOption(qry string) Option {
-	 return Option {
-		qry,0, 0,"","",0,
+func (opt Option) Response(conn *pgx.Conn) error {
+	qry := opt.qry
+	rows, _ := conn.Query(context.Background(), qry)
+	var err error 
+	
+	switch opt.optionID {
+		case 1:
+			_, err = pgx.ForEachRow(rows, []any{&opt.fname, &opt.lname}, func() error {
+			  fmt.Printf("fname: %v, lname: %v\n", opt.fname, opt.lname)
+			  return nil
+		    })
+		case 2:
+		    _, err = pgx.ForEachRow(rows,[]any{
+			  	&opt.fname, 
+			  	&opt.lname, 
+			  	&opt.age,}, func() error {
+				fmt.Printf("fname: %v, lname: %v, age:%v\n", opt.fname,opt.lname,opt.age)
+				return nil
+			})
+		default:
+			_, err = pgx.ForEachRow(rows, []any{&opt.fname, &opt.lname}, func() error {
+			  fmt.Printf("fname: %v, lname: %v\n", opt.fname, opt.lname)
+			  return nil
+		    })
 	}
+	
+	return verifyForEachRowError(err)
 }
 
 func main() {
 	
-    uri := "postgres://user:secret@ipaddr:5432/databasename"
+	uri:= "postgres://andyperalta@localhost/PeopleDB"
 	os.Setenv("DATABASE_URL", uri)
-    
+  
 	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to a database: %v\n", err)
@@ -98,38 +88,17 @@ func main() {
 	)
 	fmt.Scanf("%d\n", &argv)
 	
-
-	
+	var rqst Responder
 	switch argv {
 	case 1:
 		sqlCMD := "SELECT firstname,lastname FROM People"
-		opt := createOption(sqlCMD)
-		opt.optionId = 1 
-		opt.Response(conn)
+		rqst = createOption(sqlCMD, 1)
     case 2:
     	sqlCMD := "SELECT firstname,lastname, age FROM People"
-		opt := createOption(sqlCMD)
-		opt.optionId = 2 
-		opt.Response(conn)
-	case 3:
-		sqlCMD := "SELECT * FROM People"
-		opt := createOption(sqlCMD)
-		opt.optionId = 3 
-		opt.Response(conn)
-	case 4:
-		sqlCMD := "SELECT * FROM People"
-		opt := createOption(sqlCMD)
-		opt.optionId = 4 
-		opt.Response(conn)	
-	case 5:
-		sqlCMD := "SELECT * FROM People"
-		opt := createOption(sqlCMD)
-		opt.optionId = 5 
-		opt.Response(conn)
+    	rqst = createOption(sqlCMD, 2)
 	default:
-		sqlCMD := "SELECT * FROM People"
-		opt := createOption(sqlCMD)
-		opt.optionId = 1 
-		opt.Response(conn)	
+		sqlCMD := "SELECT firstname,lastname FROM People"
+		rqst = createOption(sqlCMD, 1)
 	}
+	rqst.Response(conn)
 }
