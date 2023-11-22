dbc7550e9452070c6e6ea30aa77a5b680b29a300
Andy Peralta <aperalta686@gmail.com>
2023-11-21 22:57:27 -0500

    Rewrite code using pgxscan wip  - rewrote query selector with other queries in mind   - added "query" function to choose between different queries   	- added helper function "loopFields" to print query results   - created Option struct to implment Stringer interface for "loopFields"  - first query option is functional but the rest are a work in progress.

--- peopleAPI/index.go
+++ peopleAPI/index.go
@@ -11,9 +11,73 @@ import (
 
 type People struct {
 	Fname string `db:"firstname"`
-	Lname string
-	ID int
-	Age int
+	Lname string `db:"lastname"`
+	ID int       `db:"id"`
+	Age int      `db:"age"`
+}
+
+type Option struct {
+	ch int // numbered choice for query
+	People 
+}
+
+//helper funct
+func (op Option) String() string{
+	choice:= op.ch
+	
+	var str string
+	switch choice {
+	  case 1:
+	  	str = fmt.Sprintf("%v, %v", op.Fname, op.Lname)
+	  case 2:
+	  	str = fmt.Sprintf("%v", op.Fname)	
+	  case 3:
+	  	str = fmt.Sprintf("%v", op.Fname)	
+	  case 4:
+	  	str = fmt.Sprintf("%v", op.Fname)	
+	  case 5:
+	  	str = fmt.Sprintf("%v", op.Fname)	
+	}
+
+	return str
+}
+
+func loopFields (ch int, users[]*People, err error) error {
+  if err != nil {
+	  fmt.Fprintf(os.Stderr, "Error Querying Row %v\n", err)
+	  os.Exit(1)
+  }
+  
+  for _, person := range users {
+  	opt := Option{ch, *person}
+	fmt.Println(opt)
+  }
+  return nil 
+}
+
+func query(choice int, ctx context.Context, 
+	db  *pgxpool.Pool, users []*People,) error {
+
+	var err error
+	switch choice {
+	 case 1:
+	 	err = pgxscan.Select(
+		ctx, db, &users, "SELECT firstname, lastname FROM People",)
+	case 2:
+	 	err = pgxscan.Select(
+		ctx, db, &users, "SELECT firstname, lastname FROM People",)
+	case 3:
+	 	err = pgxscan.Select(
+		ctx, db, &users, "SELECT firstname FROM People",)
+	case 4:
+	 	err = pgxscan.Select(
+		ctx, db, &users, "SELECT firstname FROM People",)
+	case 5:
+	 	err = pgxscan.Select(
+		ctx, db, &users, "SELECT firstname FROM People",)
+	}
+	
+	return loopFields(choice, users, err)
 }
 
 func main() {
@@ -29,28 +93,21 @@ func main() {
 	}
 	defer db.Close()
 
+    var choice int
+	fmt.Println("Select a query to ask the People's database given the following Options")
+	fmt.Println(
+		"Type '1' to get each user's name\n" +
+		"Type '2' to get each user's name and age\n" +
+		"Type '3' Compare the age between 2 users\n" +
+		"Type '4' to get the name of each user and their id\n" +
+		"Type '5' to get each user's last name and age",
+	)
+	fmt.Scanf("%d\n", &choice)
 
-    // var choice int
-// 	fmt.Println("Select a query to ask the People's database given the following Options")
-// 	fmt.Println(
-// 		"Type '1' to get each user's name\n" +
-// 		"Type '2' to get each user's name and age\n" +
-// 		"Type '3' Compare the age between 2 users\n" +
-// 		"Type '4' to get the name of each user and their id\n" +
-// 		"Type '5' to get each user's last name and age",
-// 	)
-// 	fmt.Scanf("%d\n", &choice)
-
-	
 	var users []*People
-	if err := pgxscan.Select(
-		ctx, db, &users, "SELECT firstname FROM People",)
-	err != nil {
-		fmt.Fprintf(os.Stderr, "Error Querying Row %v\n", err)
-		os.Exit(1)
-	}
-
-	for _, person := range users {
-		fmt.Printf("%v\n", person.Fname)
+	err = query(choice, ctx, db, users)
+	if err != nil {
+	  fmt.Fprintf(os.Stderr, "Error Querying Row %v\n", err)
+	  os.Exit(1)
 	}
 }
